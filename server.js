bef59dfff44176ab2828070aeb0449094f24687e
Michael Phelps <michaeljphelps1978@gmail.com>
2020-02-25 14:35:24 -0600

    Helps if I save files. Also added client app that is nowhere near ready

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ server.use(
     name: process.env.SESSION_NAME ||'CoolLoginApp',
     secret: process.env.SESSION_SECRET || '$2y$08$4RFEtq2KMkGFMD.BFpj17uHr9Tmb1Jl5eYLURaUQHfqWyhlvbCcSi',
     cookie: {
-      maxAge: 1000 * 60 * 30, 
+      maxAge: 1000 * 60 * 30,
       secure: false,
     },
     httpOnly: true,
@@ -20,7 +20,6 @@ server.use(
     saveUninitialized: false,
   })
 );
-
 middleware(server)
 
 server.use ('/api', apiRouter)
