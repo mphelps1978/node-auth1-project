07fff08756f71e6c80ecdba137210147e3ed6c77
Michael Phelps <michaeljphelps1978@gmail.com>
2020-02-25 14:41:51 -0600

    Session check passed to authorization middleware.

--- auth/restricted.js
+++ auth/restricted.js
@@ -4,25 +4,8 @@ const Users = require('../users/users-model')
 
 
 module.exports = (req, res, next) => {
-  let { username, password } = req.headers;
-
-  if(username && password){
-
-      Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next()
-
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(({name, message, stack}) => {
-        console.log(name, message, stack);
-
-        res.status(500).json({error: 'There was an error'});
-      });
+  if (req.session && req.session.loggedIn === true) {
+    next();
     }else {
       res.status(400).json({message: 'You must be logged in to do that'})
 
