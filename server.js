10732b2f5079831e51d392af4d9ed4149a23eebb
Michael Phelps <michaeljphelps1978@gmail.com>
2020-02-25 14:34:27 -0600

    setup session variable using express-session

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const session = require('express-session');
 
 const middleware = require('./api/mware')
 
 const apiRouter = require('./api/apiRouter');
 
-
 const server = express();
+
+server.use(
+  session({
+    name: process.env.SESSION_NAME ||'CoolLoginApp',
+    secret: process.env.SESSION_SECRET || '$2y$08$4RFEtq2KMkGFMD.BFpj17uHr9Tmb1Jl5eYLURaUQHfqWyhlvbCcSi',
+    cookie: {
+      maxAge: 1000 * 60 * 30, 
+      secure: false,
+    },
+    httpOnly: true,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 middleware(server)
 
