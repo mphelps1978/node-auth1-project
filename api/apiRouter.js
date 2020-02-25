3477062683a57b96e229567ac69994499b08a6de
Michael Phelps <michaeljphelps1978@gmail.com>
2020-02-25 14:39:35 -0600

    passed session storage to login component. Currently only works in memory

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -37,6 +37,10 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        
+        req.session.loggedIn = true;
+        req.session.userName = user.username;
+
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
